8aaaaeffeb5b95a8964ffc39a544fe29fe37b19a
Lucas <lucasgustavo8830@gmail.com>
2020-12-01 19:56:43 -0300

    ultima v bd

--- dssd.sql
+++ dssd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 30-11-2020 a las 20:11:40
+-- Tiempo de generación: 01-12-2020 a las 22:56:10
 -- Versión del servidor: 5.7.31
 -- Versión de PHP: 7.3.21
 
@@ -111,6 +111,7 @@ CREATE TABLE IF NOT EXISTS `protocolos` (
   `id_proyecto` int(11) NOT NULL,
   `estado` varchar(255) NOT NULL,
   `es_local` int(11) DEFAULT NULL,
+  `borrado` int(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_protocolo`),
   KEY `protocolo_responsable` (`id_responsable`),
   KEY `protocolo_proyecto` (`id_proyecto`)
@@ -120,19 +121,19 @@ CREATE TABLE IF NOT EXISTS `protocolos` (
 -- Volcado de datos para la tabla `protocolos`
 --
 
-INSERT INTO `protocolos` (`id_protocolo`, `nombre`, `id_responsable`, `fecha_inicio`, `fecha_fin`, `orden`, `puntaje`, `id_proyecto`, `estado`, `es_local`) VALUES
-(1, 'protocolo_a', 4, '2020-12-01', '2020-12-02', 1, 0, 5, 'pendiente', 0),
-(2, 'protocolo_b', 2, '2020-12-02', '2020-12-03', 2, 0, 5, 'pendiente', 0),
-(3, 'protocolo_c', 3, '2020-12-03', '2020-12-04', 3, 0, 5, 'pendiente', 1),
-(4, 'protocolo_d', 2, '2020-12-04', '2020-12-05', 1, 0, 5, 'pendiente', 1),
-(5, 'protocolo_e', 2, '2020-12-04', '2020-12-05', 1, 0, 5, 'pendiente', 1),
-(6, 'protocolo_a', 3, '2020-12-06', '2020-12-07', 1, 0, 7, 'pendiente', 0),
-(7, 'protocolo_b', 3, '2020-12-07', '2020-12-08', 2, 0, 7, 'pendiente', 0),
-(8, 'protocolo_c', 3, '2020-12-08', '2020-12-09', 1, 0, 7, 'pendiente', 1),
-(9, 'protocolo_d', 4, '2020-12-09', '2020-12-12', 1, 0, 7, 'pendiente', 1),
-(10, 'protocolo_a', NULL, NULL, NULL, NULL, 0, 8, 'pendiente', NULL),
-(11, 'protocolo_b', 4, '2020-11-06', '2020-11-20', 4, 0, 8, 'pendiente', 0),
-(12, 'protocolo_c', NULL, NULL, NULL, NULL, 0, 8, 'pendiente', NULL);
+INSERT INTO `protocolos` (`id_protocolo`, `nombre`, `id_responsable`, `fecha_inicio`, `fecha_fin`, `orden`, `puntaje`, `id_proyecto`, `estado`, `es_local`, `borrado`) VALUES
+(1, 'protocolo_a', 4, '2020-12-01', '2020-12-02', 1, 8, 5, 'completado', 0, 0),
+(2, 'protocolo_b', 2, '2020-12-02', '2020-12-03', 2, 6, 5, 'completado', 0, 0),
+(3, 'protocolo_c', 3, '2020-12-03', '2020-12-04', 3, 4, 5, 'completado', 1, 0),
+(4, 'protocolo_d', 2, '2020-12-04', '2020-12-05', 1, 0, 5, 'pendiente', 1, 0),
+(5, 'protocolo_e', 2, '2020-12-04', '2020-12-05', 1, 0, 5, 'pendiente', 1, 0),
+(6, 'protocolo_a', 3, '2020-12-06', '2020-12-07', 1, 0, 7, 'pendiente', 0, 0),
+(7, 'protocolo_b', 3, '2020-12-07', '2020-12-08', 2, 0, 7, 'pendiente', 0, 0),
+(8, 'protocolo_c', 3, '2020-12-08', '2020-12-09', 1, 0, 7, 'pendiente', 1, 0),
+(9, 'protocolo_d', 4, '2020-12-09', '2020-12-12', 1, 0, 7, 'pendiente', 1, 0),
+(10, 'protocolo_a', 4, '2020-12-09', '2020-12-12', 1, 4, 8, 'completado', 1, 0),
+(11, 'protocolo_b', 4, '2020-11-06', '2020-11-20', 4, 5, 8, 'completado', 0, 0),
+(12, 'protocolo_c', 4, '2020-11-01', '2020-11-08', 1, 6, 8, 'completado', 1, 0);
 
 -- --------------------------------------------------------
 
@@ -148,6 +149,7 @@ CREATE TABLE IF NOT EXISTS `proyectos` (
   `fecha_fin` date NOT NULL,
   `id_responsable` int(11) NOT NULL,
   `case_id` int(11) DEFAULT NULL,
+  `borrado` int(2) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_proyecto`),
   KEY `proyecto_responsable` (`id_responsable`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
@@ -156,10 +158,10 @@ CREATE TABLE IF NOT EXISTS `proyectos` (
 -- Volcado de datos para la tabla `proyectos`
 --
 
-INSERT INTO `proyectos` (`id_proyecto`, `nombre`, `fecha_inicio`, `fecha_fin`, `id_responsable`, `case_id`) VALUES
-(5, 'vacuna_covid', '2020-12-01', '2020-12-05', 5, 5005),
-(7, 'vacuna_sarampion', '2020-12-06', '2020-12-12', 1, NULL),
-(8, 'vacuna_ebola', '2020-12-13', '2020-12-19', 5, NULL);
+INSERT INTO `proyectos` (`id_proyecto`, `nombre`, `fecha_inicio`, `fecha_fin`, `id_responsable`, `case_id`, `borrado`) VALUES
+(5, 'vacuna_covid', '2020-12-01', '2020-12-05', 5, 6002, 0),
+(7, 'vacuna_sarampion', '2020-12-06', '2020-12-12', 1, NULL, 0),
+(8, 'vacuna_ebola', '2020-12-13', '2020-12-19', 5, NULL, 1);
 
 -- --------------------------------------------------------
 
